58787ddd76d759bca24be7f618a7f4203c0758eb
BDloverHS <gustn020702naver.com@gmail.com>
2025-01-20 16:43:28 +0900

    초기 업로드

---
+++ src/main/java/org/advisor/AdvisorBankApplication.java
@@ -0,0 +1,13 @@
+package org.advisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisorBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvisorBankApplication.class, args);
+	}
+
+}
